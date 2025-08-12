77cedbac193eb24a6bac17541cf7919787f5597d
Francois Chastanet <francois.chastanet@crossknowledge.com>
2025-08-12 22:20:05 +0200

    better sleep signature

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -3,7 +3,7 @@ export class Utils {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  static async sleep(timeout: number, fn: Function, ...args: any[]) {
+  static async sleep<T extends (...args: any[]) => any>(timeout: number, fn: T, ...args: Parameters<T>): Promise<ReturnType<T>> {
     await this.timeout(timeout);
     return fn(...args);
   }
